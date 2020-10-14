e68d3de96baf9099112e5b9844abf41fc5bab583
Ajarnnotes <ajarnnotes@gmail.com>
2020-10-15 00:27:22 +0700

    Add BannerBlock to Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+import withHelmet from '../util/withHelmet'
+import BannerBlock from '../components/BannerBlock'
+
+const Content = styled.div`
+  padding: 8rem;
+`
+
+function Home () {
+  return (
+    <div>
+      <BannerBlock src={backgroundImage}>
+        <Title>PartyGames</Title>
+        <Subtitle></Subtitle>
+      </BannerBlock>
+      <Content>Home Page</Content>
+    </div>
+  )
+}
+export default withHelmet('Ajarn | Blog App')(Home)
