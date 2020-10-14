4450de32ae6e737996087aed22eacb3004f408dc
Supatat Singkomfu <Supatat Singkomfu>
2020-10-15 06:16:11 +0700

    Add profile page

---
+++ src/pages/Profile.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import withHelmet from '../util/withHelmet'
+import backgroundImage from '../assets/rockman.jpg'
+import BannerBlock from '../components/BannerBlock'
+import BoxProfile from '../components/BoxProfile'
+
+function Post () {
+    return (
+      <div>
+        <BannerBlock src={backgroundImage}>
+            <BoxProfile/>
+        </BannerBlock>
+      </div>
+    )
+  }
+
+  export default withHelmet('PartyGames')(Post)
+  
