a7dd371c7dcc93aa61c6755e3478d4ff2805084e
Supatat Singkomfu <Supatat Singkomfu>
2020-10-15 06:17:07 +0700

    Add post page

---
+++ src/pages/Post.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import withHelmet from '../util/withHelmet'
+import backgroundImage from '../assets/rockman.jpg'
+import BannerBlock from '../components/BannerBlock'
+import FormPost from '../components/FormPost'
+
+function Post () {
+    return (
+      <div>
+        <BannerBlock src={backgroundImage}>
+            <FormPost/>
+        </BannerBlock>
+      </div>
+    )
+  }
+
+  export default withHelmet('PartyGames')(Post)
+  
