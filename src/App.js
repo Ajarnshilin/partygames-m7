1e21107ed2c271355f54aefecec933fcaa5d9efb
Ajarnnotes <ajarnnotes@gmail.com>
2020-10-15 00:46:08 +0700

    Add NavBar and Home page to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import routes from './config/routes'
+import withHelmet from './util/withHelmet'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Router>
+        <NavBar />
+        <Suspense fallback='...loading'>
+          <Switch>
+            {Object.keys(routes).map(routeKey => (
+              <Route key={routeKey} {...routes[routeKey]} />
+            ))}
+          </Switch>
+        </Suspense>
+      </Router>
     </div>
   );
 }
 
-export default App;
+export default withHelmet('PartyGames')(App)
