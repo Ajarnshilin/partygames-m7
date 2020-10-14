02833b1815c712e3ab6cc3badcaaebb12ed9c107
Ajarnnotes <ajarnnotes@gmail.com>
2020-10-15 01:09:20 +0700

    Add GlobalStyle

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import routes from './config/routes'
 import withHelmet from './util/withHelmet'
 import NavBar from './components/NavBar'
+import GlobalStyle from './components/GlobalStyle'
 
 function App() {
   return (
