6e280a9b7d4d8f6fff283f8ea580a9b2f0f152cf
Supatat Singkomfu <Supatat Singkomfu>
2020-10-15 07:19:39 +0700

    Edit Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,20 @@ import withHelmet from '../util/withHelmet'
 import backgroundImage from '../assets/rockman.jpg'
 import BannerBlock from '../components/BannerBlock'
 import { Title, Subtitle } from '../components/Typography'
+import Content from '../components/Content'
 
-const Content = styled.div`
-  padding: 8rem;
+const PostList = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    padding: 50px 0;
+    & a {
+    text-decoration: none;
+    }
 `
 
 function Home () {
@@ -16,7 +27,11 @@ function Home () {
         <Title>PartyGames</Title>
         <Subtitle></Subtitle>
       </BannerBlock>
-      <Content>Home Page</Content>
+      <PostList>
+        <Content></Content>
+        <Content></Content>
+        <Content></Content>
+      </PostList>
     </div>
   )
 }
