af17817e36cd1194b379b734f1cc152f2343367a
Supatat Singkomfu <Supatat Singkomfu>
2020-10-23 03:09:01 +0700

    Edit Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 import withHelmet from '../util/withHelmet'
 import backgroundImage from '../assets/rockman.jpg'
-import BannerBlock from '../components/BannerBlock'
+import HomeBlock from '../components/HomeBlock'
 import { Title, Subtitle } from '../components/Typography'
 import Content from '../components/Content'
 import Overwatch from '../assets/games/ow.jpg'
 import Minecraft from '../assets/games/mc.jpg'
 import PubG from '../assets/games/pubg.jpg'
+import Apex from '../assets/games/apex.jpg'
+import Va from '../assets/games/va.png'
+import { Link } from 'react-router-dom'
 
 const PostList = styled.div`
     width: 100%;
@@ -17,23 +20,57 @@ const PostList = styled.div`
     justify-content: center;
     align-items: center;
     box-sizing: border-box;
-    padding: 50px 0;
+    padding: 50px;
     a {
     text-decoration: none;
     }
 `
 
-function Home () {
+const Post = styled.button`
+    width: 110px;
+    height: 40px;
+    border: none;
+    color: white;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 20px;
+    cursor: pointer;
+    background-color: #4678B0;
+    border-radius: 10px;
+    outline: none;
+    margin-top: 20px;
+    &:hover{
+    background-color: #1E3859;
+    }
+`
+
+const Center = styled.div`
+  margin: 70px 0 0 150px;
+  height: 300px;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+`
+
+function Home() {
   return (
     <>
-      <BannerBlock src={backgroundImage}>
+      <HomeBlock src={backgroundImage}>
+        <Center>
         <Title>PartyGames</Title>
-        <Subtitle></Subtitle>
-      </BannerBlock>
+        <Subtitle>You are not limited to the features of this template. It is suitable for gaming
+        site, and for any business project. Pre-packed demos will help you to quickly
+        run your new creative website.</Subtitle>
+        <Post><Link to='/Post'>POST</Link></Post>
+        </Center>
+      </HomeBlock>
       <PostList>
         <Content src={Overwatch}></Content>
         <Content src={Minecraft}></Content>
         <Content src={PubG}></Content>
+        <Content src={Apex}></Content>
+        <Content src={Va}></Content> 
       </PostList>
     </>
   )
