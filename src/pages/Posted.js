ac586df45f9536835e91287b0e6e006adca8eb7a
Supatat Singkomfu <Supatat Singkomfu>
2020-10-23 04:49:23 +0700

    Add Posted

---
+++ src/pages/Posted.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import withHelmet from '../util/withHelmet'
+import backgroundImage from '../assets/rockman.jpg'
+import BannerBlock from '../components/BannerBlock'
+import BoxDetails from '../components/BoxDetails'
+import styled from 'styled-components'
+
+const H1 = styled.h1`
+    color: #fff;
+    font-size: 30px;
+    font-weight: bold;
+    margin-top: 70px;
+`
+
+function Posted () {
+  return (
+    <div>
+      <BannerBlock src={backgroundImage}>
+        <H1>Party You've Posted</H1>
+        <BoxDetails />
+      </BannerBlock>
+    </div>
+  )
+}
+export default withHelmet('PartyGames')(Posted)
