f3cf094b5bce00a8a0b59202a6c33874dd968d68
Supatat Singkomfu <Supatat Singkomfu>
2020-10-22 18:47:48 +0700

    Edit Form Register

--- src/components/FormRegister.js
+++ src/components/FormRegister.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const Container = styled.div`
-    height: 500px;
+    height: 600px;
     width: 400px;
     background-color: white;
     border-radius: 10px;
     display: flex;
     flex-direction: column;
     align-items: center;
+    margin-top: 100px;
 `
 
 const Form = styled.form`
@@ -16,20 +18,25 @@ const Form = styled.form`
     height: 100%;
     display: flex;
     flex-direction: column;
+    a {
+        margin-top: 5px;
+        font-size: 16px;
+    }
 `
 
 const Title = styled.span`
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 100px;
+    height: 200px;
     /* margin: 500px; */
-    font-size: 3rem;
+    font-size: 2rem;
+    color: #1E3859;
 `
 
 const Input = styled.input`
     margin: 10px 0;
-    padding: 0 10px;
+    padding: 5px 10px;
     height: 30px;
     border-radius: 5px;
     border: none;
@@ -40,14 +47,29 @@ const Input = styled.input`
     }
 `
 
+const Textarea = styled.textarea`
+    height: 150px;
+    margin: 10px 0;
+    /* padding: 5px 10px; */
+    border-radius: 5px;
+    border: none;
+    border:solid 1px gray;
+    outline: none;
+    resize: none;
+    padding: 5px 10px;
+    &:focus {
+        border: 1px solid #0C1737;
+    }
+`
+
 const Button = styled.button`
-    height: 30px;
-    width: 100%;
-    background-color: #57A4CC;
-    margin-top: 20px;
+    height: 45px;
+    width: 110px;
+    background-color: #4678B0;
+    margin-top: 30px;
     border: 0;
     color: white;
-    font-size: 16px;
+    font-size: 20px;
     outline: none;
     border-radius: 5px;
     cursor: pointer;
@@ -58,38 +80,52 @@ const Button = styled.button`
 
 const Submit = styled.div`
     width: 100%;
+    height: 200px;
     display: flex;
     flex-direction: column;
     text-align: center;
-    &a {
-    margin-top: 20px;
-    height: 30px;
+    justify-content: space-between;
+    align-items: center;
+    a {
     width: 100%;
-    color: unset;
-    text-decoration: none;
+    font-size: 16px;
+    margin: 20px 0;
     }
+`
+
+const Register = styled.button`
+    border: none;
+    background-color: unset;
+    font-size: 16px;
+    color: #4678B0;
+    cursor: pointer;
+    outline: none;
 `
 
 function FormRegister() {
     return (
         <Container>
-        <Form class="form" method="POST" action="{{route('/login')}}">
-            <Title>Register</Title>
+        <Form method="POST" action="{{route('/login')}}">
+            <Title>Sign Up</Title>
 
-            <a for="username">Username</a>
-            <Input  class="input" type ="text" name="username" id="username" placeholder="e.g. JohnDoe"></Input>
+            <a>Username</a>
+            <Input type ="text" name="username" id="username" placeholder="e.g. JohnDoe"></Input>
 
-            <a for="email">Email</a>
-            <Input  class="input" type ="text" name="email" id="email" placeholder="e.g. JohnDoe@example.com"></Input>
+            <a>Email</a>
+            <Input type ="text" name="email" id="email" placeholder="e.g. JohnDoe@example.com"></Input>
 
-            <a for="password">Password</a>
-            <Input  class="input" type ="password" name="password" id="password" placeholder="********"></Input>
+            <a>Password</a>
+            <Input type ="password" name="password" id="password" placeholder="**********"></Input>
 
-            <a for="confirmPassword">Confirm Password</a>
-            <Input  class="input" type ="password" name="comfirmPassword" id="comfirmPassword" placeholder="********"></Input>
+            <a>Confirm Password</a>
+            <Input type ="password" name="comfirmPassword" id="comfirmPassword" placeholder="**********"></Input>
+
+            <a>Contacts</a>
+            <Textarea type ="textarea"></Textarea>
 
             <Submit>
-            <Button type ="submit">SUBMIT</Button>
+            <Button type ="submit">Sign Up</Button>
+            <a>Already have an account? | <Register><Link to='/Login'>Sign In</Link></Register></a>
             </Submit>
 
         </Form>
