1402996d8d40c869927821620716d828c8c129f8
Ajarnnotes <ajarnnotes@gmail.com>
2020-10-25 01:32:18 +0700

    Edit Details to dynamix route

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import withHelmet from '../util/withHelmet'
 import backgroundImage from '../assets/rockman.jpg'
 import BannerBlock from '../components/BannerBlock'
 import BoxDetails from '../components/BoxDetails'
+import { getDataById } from '../services/fetchData'
+import { useParams } from 'react-router-dom'
 
-function Login () {
+function Details (props) {
+  const [data, setData] = useState([])
+  const [user, setUser] = useState([])
+  const { id } = useParams()
+
+  useEffect(() => {
+    getDataById('posts',id).then(response => setData(response))
+    getDataById('clients',id).then(response => setUser(response))
+  },[])
+
+  console.log(data)
+  console.log(user)
+
   return (
-    <div>
-      <BannerBlock src={backgroundImage}>
-        <BoxDetails />
-      </BannerBlock>
-    </div>
+    <>
+      <div>
+        <BannerBlock src={backgroundImage}>
+          <BoxDetails src={data.title} title={data.title} user={user.username} party={data.party_size} details={data.details} date={data.date}/>
+        </BannerBlock>
+      </div>
+    </>
   )
 }
-export default withHelmet('PartyGames')(Login)
+export default withHelmet('PartyGames')(Details)
