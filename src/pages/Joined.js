e0addb11c066bc2ce7d20a1b02156bd32060981e
Supatat Singkomfu <Supatat Singkomfu>
2020-10-26 01:19:49 +0700

    Edit background in Joined page

--- src/pages/Joined.js
+++ src/pages/Joined.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import withHelmet from '../util/withHelmet'
-import backgroundImage from '../assets/rockman.jpg'
+import backgroundImage from '../assets/background.gif'
 import BannerBlock from '../components/BannerBlock'
 import BoxJoined from '../components/BoxJoined'
 import { getDataById } from '../services/fetchData'
