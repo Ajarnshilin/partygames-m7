9442066cee0c2446e369d2ec2d0a3cf3f3eaa75a
Ajarnnotes <ajarnnotes@gmail.com>
2020-10-26 07:41:37 +0700

    Add register API

---
+++ src/services/registerAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+export default function RegisterAPI(values,setErrors) {
+  console.log(values)
+    axios ( {
+      url: 'http://localhost:3333/register',
+      method: 'post',
+      data: {
+        "username": values.username,
+        "email": values.email,
+        "password": values.password,
+        "contact": values.contact
+      },
+      responseType: 'json', 
+    }) .then(response => {
+      console.log(response) 
+
+    })
+}
